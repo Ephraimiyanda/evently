8d63c5555ee8e9c120d4c305d811bf93e2519823
Ephraimiyanda <iyandaephraim@gmail.com>
2025-06-04 22:40:49 +0100

    feature : installed missing dependencies

--- components/cards/feature-cards.tsx
+++ components/cards/feature-cards.tsx
@@ -1,5 +1,5 @@
 import { AnimationControls, motion } from "framer-motion";
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody } from "@heroui/card";
 
 import { featureTypes } from "@/types/features";
 
